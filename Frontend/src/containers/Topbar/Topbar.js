df6166d4d43685e4153302e3e31d74edbd77a43c
Amadou Boubacar Niang <amadouniang2001@gmail.com>
2026-01-20 21:30:29 +0000

    changes

--- Frontend/src/containers/Topbar/Topbar.js
+++ Frontend/src/containers/Topbar/Topbar.js
@@ -1,5 +1,5 @@
 import React from "react";
-import logo from '../../images/download YouTube Thumbnail 2.png'
+import logo from '../../images/download YouTube Thumbnail.png'
 const Topbar = () => {
     return (
         <div
